0d87cb5b1d42f3bac9035918d04dd91fb4cdc090
PetrenkoYevhenii <114873694+PetrenkoYevhenii@users.noreply.github.com>
2022-12-14 11:06:49 +0200

    login

--- Login.java
+++ Login.java
@@ -2,14 +2,17 @@ import java.awt.Font;
 
 import javax.swing.*;
 
+
+
 import java.awt.Color;
 import java.awt.event.*;
+import java.sql.*;
 
 public class Login extends JFrame implements ActionListener {
 
     JButton login, signup;
-    JTextField cardNumField, cardPinField;
-    JPasswordField pinTexField;
+    JTextField cardnoField, passwordTextField;
+    JPasswordField passwordField;
 
     Login() {
 
@@ -22,23 +25,23 @@ public class Login extends JFrame implements ActionListener {
 
         getContentPane().setBackground(Color.WHITE);
 
-        JLabel cardnum = new JLabel("Номер карти");
-        cardnum.setBounds(120, 150, 150, 40);
-        cardnum.setFont(new Font("Raleway", Font.BOLD, 28));
-        add(cardnum);
+        JLabel cardno = new JLabel("Номер карти");
+        cardno.setBounds(120, 150, 200, 40);
+        cardno.setFont(new Font("Raleway", Font.BOLD, 28));
+        add(cardno);
 
-        cardNumField = new JTextField();
-        cardNumField.setBounds(200, 150, 250, 40);
-        add(cardNumField);
+        cardnoField = new JTextField();
+        cardnoField.setBounds(300, 150, 250, 40);
+        add(cardnoField);
 
-        JLabel cardpin = new JLabel("Пін");
-        cardpin.setBounds(120, 220, 400, 40);
-        cardpin.setFont(new Font("Raleway", Font.BOLD, 28));
-        add(cardpin);
+        JLabel password = new JLabel("Пароль");
+        password.setBounds(120, 220, 400, 40);
+        password.setFont(new Font("Raleway", Font.BOLD, 28));
+        add(password);
 
-        cardPinField = new JPasswordField();
-        cardPinField.setBounds(200, 220, 250, 40);
-        add(cardPinField);
+        passwordTextField = new JPasswordField();
+        passwordTextField.setBounds(300, 220, 250, 40);
+        add(passwordTextField);
 
         login = new JButton("Увійти");
         login.setBounds(300, 300, 100, 30);
@@ -58,12 +61,36 @@ public class Login extends JFrame implements ActionListener {
     }
 
     public void actionPerformed(ActionEvent ae) {
+        try{
+        
         if (ae.getSource() == login) {
+            conmysql c = new conmysql();
+            String cardno = cardnoField.getText();
+            String password = passwordTextField.getText();
+            String Query = "select * from login where cardno = '"+cardno+"' and password = '"+password+"'";
+            try{
+                ResultSet rs = c.s.executeQuery(Query);
+                if(rs.next()){
+                    setVisible(false);
+                    new transactionsmenu(password).setVisible(true);
+                
+                }else{
+                    JOptionPane.showMessageDialog(null, "Не вірний номер карти чи пароль");
+                }
+
+            }catch (Exception e){
+                System.out.println(e);
+            }
+            
 
         } else if (ae.getSource() == signup) {
             setVisible(false);
             new signup1().setVisible(true);
 
+        }
+
+        }catch (Exception e){
+            System.out.println(e);
         }
 
     }
